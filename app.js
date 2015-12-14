f38abc4a47084e1622e452113516048759111f98
Aubrey Ford <musicaubrey@gmail.com>
2015-12-14 15:47:54 -0700

    completed addingcards

--- app.js
+++ app.js
@@ -1,15 +1,38 @@
 var express = require('express');
 var app = express()
 var http = require('http').Server(app);
-
+var io = require('socket.io')(http)
 
 app.set("view engine", "ejs");
-
 app.use(express.static("public"));
+
+
 
 app.get('/', function(req, res){
   res.render('index');
 });
+
+
+io.on('connection', function(socket){
+  console.log("a user connected")
+
+  socket.on('sendcard', function(res){
+    console.log(res)
+    io.emit('sendcard', res)
+  })
+
+
+  socket.on('disconnect', function(){
+    console.log("a user disconnected")
+  })
+});
+
+
+
+
+
+
+
 
 
 
--- public/main.js
+++ public/main.js
@@ -1,6 +1,7 @@
 
 $(function(){
 
+var socket = io()
 
 function getgifs(){
       return $.ajax({
@@ -14,7 +15,23 @@ for (var i = 0; i < 4; i++) {
   var callrandom = getgifs()
   callrandom.done(function(res){
     console.log(res)
-  $(".hand").append("<img data-player='player1' src='"+res.data.image_url+"'>")
+  $(".hand").append("<img data-player='player1' class='handcard' src='"+res.data.image_url+"'>")
   })
 }
+
+var canhand = true
+$(document).on('click', ".handcard", function(){
+    if (canhand === true){
+    socket.emit('sendcard', $(this)[0].outerHTML)
+    }
+    canhand = false
+})
+
+socket.on('sendcard', function(res){
+  // var newcard = res.removeClass("handcard").addClass("boardcard")
+  $('.board').append(res)
+  $('.board').children().removeClass("handcard").addClass("boardcard")
+})
+
+
 })
