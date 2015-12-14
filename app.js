525c2131c9c4cfbac7e9d19f19fc1502dcd7a370
Aubrey Ford <musicaubrey@gmail.com>
2015-12-14 14:48:04 -0700

    gifs incoming

--- app.js
+++ app.js
@@ -1,15 +1,17 @@
-var app = require('express')();
+var express = require('express');
+var app = express()
 var http = require('http').Server(app);
 
 
-app.set("view engine", "ejs")
+app.set("view engine", "ejs");
 
-app.use(express.static("public"))
+app.use(express.static("public"));
 
 app.get('/', function(req, res){
-  // res.send('<h1>Hello world</h1>')
   res.render('index');
 });
+
+
 
 http.listen(3000, function(){
   console.log('listening on *:3000');
--- public/main.js
+++ public/main.js
@@ -1,26 +1,20 @@
-alert('working')
 
-// $(function(){
-//
-// alert('working')
-//
-// var getgifs = $.ajax({
-//       url="http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC",
-//       method="GET",
-//       dataType="json"
-//     })
-//
-//
-// getgifs.done(function(res){
-//   console.log(res);
-//   $(".hand").append("<img src='"+res.url+"'>")
-// })
-
-// getgifs.done(function(res){
-//   console.log(res);
-//   $(".hand").append("<img src='"+res.url+"'>")
-// })
+$(function(){
 
 
+function getgifs(){
+      return $.ajax({
+      url:"http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC",
+      method:"GET",
+      dataType:"json"
+    })
+  }
 
-// })
+for (var i = 0; i < 4; i++) {
+  var callrandom = getgifs()
+  callrandom.done(function(res){
+    console.log(res)
+  $(".hand").append("<img data-player='player1' src='"+res.data.image_url+"'>")
+  })
+}
+})
