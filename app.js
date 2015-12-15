53526b2ee08496a7428b9f114856afa3198ea35a
Aubrey Ford <musicaubrey@gmail.com>
2015-12-15 03:35:11 -0700

    most mvp functionality complete

--- app.js
+++ app.js
@@ -13,44 +13,64 @@ app.get('/', function(req, res){
 });
 
 
+
+
 var usercounter = 1
 var roomid
-
 io.on('connection', function(socket){
-  console.log("a user connected")
-//////////////////
-  var newid = "player" + usercounter
 
-  if (usercounter === 1){
-    roomid = socket.id
-  }else{
-    socket.join(roomid)
-  }
+    var newid = usercounter
 
-  if (usercounter !== 4) {
-    usercounter++
-  }else{
-    usercounter = 1;
-    io.emit('startgame', "player1")
-  }
+    if (usercounter === 1){
+      roomid = socket.id
+    }else{
+      socket.join(roomid)
+    }
+
+
+    socket.emit('userId', {"newid" : newid, "roomid" : roomid})
+
+    if (usercounter !== 4) {
+      usercounter++
+    }else{
+      // io.to(roomid).emit('updatescore', [0,0,0,0])
+      usercounter = 1;
+    }
+
 ///////////////
-  socket.emit('userId', {"newid" : newid, "roomid" : roomid})
-
-  socket.on('sendcard', function(res){
-    io.to(res.room).emit('sendcard', res.card)
-  })
 
 
-  socket.on('disconnect', function(){
-    console.log("a user disconnected")
-  })
+    socket.on('sendcard', function(res){
+      io.to(res.room).emit('sendcard', res.card)
+    })
+
+    socket.on('selection', function(res){
+
+      var score = res.score
+      switch (res.playerwinner) {
+        case 1: score[0]++
+        break;
+        case 2: score[1]++
+        break;
+        case 3: score[2]++
+        break;
+        case 4: score[3]++
+        break;
+      }
+      io.to(res.room).emit('updatescore', score)
+
+      io.to(res.room).emit('newgame')
+    })
+
+
+
+
+
+
+
+
+
 });
-
-
-
-
-
-
 
 
 
--- public/main.js
+++ public/main.js
@@ -11,6 +11,7 @@ function getgifs(){
     })
   }
 
+var score = [0,0,0,0]
 
 var playerId
 var roomId
@@ -21,23 +22,36 @@ socket.on('userId', function(id){
   console.log(roomId);
 })
 
+function deal(){
 for (var i = 0; i < 4; i++) {
+  $(".hand").children().remove()
+  $(".board").children().remove()
+
   var callrandom = getgifs()
   callrandom.done(function(res){
-  $(".hand").append("<img data-player='"+playerId+"' class='handcard' src='"+res.data.image_url+"'>")
+  $(".hand").append("<img data-player="+playerId+" class='handcard' src='"+res.data.image_url+"'>")
   })
 }
+}
+deal()
 
 
-var selector
-socket.on('startgame', function(res){
-  selector = res
+var selector = 1
+socket.on('newgame', function(){
+
+  if (selector === 4){
+    selector = 1
+  }else{
+  selector++
+  }
+  canhand=true
+  deal()
   console.log(selector)
 })
 ///////
 var canhand = true
 $(document).on('click', ".handcard", function(){
-    if (canhand === true){
+    if (canhand === true && selector !== playerId){
     socket.emit('sendcard', {"card":$(this)[0].outerHTML,"room":roomId})
     }
     canhand = false
@@ -48,14 +62,33 @@ socket.on('sendcard', function(card){
   $('.board').children().removeClass("handcard").addClass("boardcard")
 })
 /////////////////
+
+
+var canboard = true
 $(document).on('click', '.boardcard', function(){
-    if (selector === playerId){
-      socket.emit('selection', {"card":$(this).})
+    if (selector === playerId && canboard === true){
+
+      socket.emit('selection', {"playerwinner":$(this).data("player"), 'room':roomId, 'score':score})
+
+      // console.log($(this).data("player"))
+
+      // canboard = false
+
     }
 })
 
 
 
+
+
+socket.on('updatescore', function(scoreincoming){
+    score = scoreincoming
+    $(".score1").html("Player 1: " + score[0])
+    $(".score2").html("Player 2: " + score[1])
+    $(".score3").html("Player 3: " + score[2])
+    $(".score4").html("Player 4: " + score[3])
+    console.log(score)
+})
 
 
 
