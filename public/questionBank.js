72be6edc4947613c4a0ea6f904774b65259e02cf
Aubrey Ford <musicaubrey@gmail.com>
2015-12-16 10:41:27 -0700

    added more questions

--- public/questionBank.js
+++ public/questionBank.js
@@ -25,11 +25,57 @@ questions: [
 
 "I'm sorry professor, but I couldn't complete my homework because of _______________.",
 
+"White people like __________________.",
 
+"Next from J.K. Rowling:  Harry Potter and the Chamber of ______________________.",
 
+"During sex, I like to think about ________________________.",
 
+"What will always get you laid?",
 
+"What did I bring back from Mexico?",
 
+"How am I maintaining my relationship status?",
+
+"___________________________.  It's a trap!",
+
+"After the earthquake, Sean Penn brought _____________________ to the people of Haiti.",
+
+"But before I kill you, Mr. Bon, I must show you ____________________.",
+
+"What gives me uncontrollable gas?",
+
+"The new Chevy Tahoe.  With the power and space to take _________________ everywhere you go.",
+
+"The class field trip was completely ruined by ______________________.",
+
+"When Pharaoh remained unmoved, Moses called down a Plague of __________________.",
+
+"What's my secret power?",
+
+"What's there a ton of in heaven?",
+
+"What did Vin Diesel eat for dinner?",
+
+"Why am I sticky?",
+
+"What gets better with age?",
+
+"Daddy, why is mommy crying?",
+
+"What's Teach for America using to inspire inner city students to succeed?",
+
+"I do not know with what weapons World War III will be fought with, but World War IV will be fought with _________________________.",
+
+"What's George W. Bush thinging about right now?",
+
+"What am I giving up for Lent?",
+
+"What's my anti-drug?",
+
+"What never fails to liven up the party?",
+
+"When I was tripping on acid, the lamp turned into ___________________."
 
 
 
