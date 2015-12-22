16ed9a3e1c40a62830e47ee2943548eb46149f8a
Aubrey Ford <musicaubrey@gmail.com>
2015-12-22 14:04:04 -0700

    heroku attempt1

--- app.js
+++ app.js
@@ -81,9 +81,9 @@ io.on('connection', function(socket){
 
 
 
-
-http.listen(3000, function(){
-  console.log('listening on *:3000');
+var port = process.env.PORT || 8080
+http.listen(port, function(){
+  console.log('listening on' + port);
 });
 
 
